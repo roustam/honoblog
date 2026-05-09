4ce5a53d2cdde103ac1bbd4e9e2409d62df858b6
Roustam <roustam.ts@gmail.com>
2026-05-09 18:57:37 +0400

    fix: vercel edge to normal format

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,7 +1,11 @@
-export const runtime = "edge";
+export const config = {
+  runtime: "edge",
+};
 
 import app from "../src/app";
 
 export default async function handler(request: Request) {
+  console.log("Request URL:", request.url);
+  console.log("Request method:", request.method);
   return app.fetch(request);
 }
