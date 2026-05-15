46a362b13e68f05a867b8c8b7b12c9f3e5478c22
Roustam <roustam.ts@gmail.com>
2026-05-15 19:30:32 +0400

    test for api dir index

--- api/index.ts
+++ api/index.ts
@@ -5,4 +5,6 @@ import { handle } from 'hono/vercel'
 
 export const config = { runtime: 'nodejs' }
 
+app.get('/hello', (c) => c.json({ message: 'Hello from 123 321 Hono!' }))
+
 export default handle(app)
