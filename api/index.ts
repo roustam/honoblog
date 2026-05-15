26880ac46f9767798572a76334c66e67e533a52d
Roustam <roustam.ts@gmail.com>
2026-05-15 19:42:23 +0400

    vercel config rewrite changes & test for api dir

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,23 @@
-// api/index.ts
-// workaround for vercel 
-import app from '../src/index'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { cors } from "hono/cors"
 
 export const config = { runtime: 'nodejs' }
 
-app.get('/hello', (c) => c.json({ message: 'Hello from 123 321 Hono!' }))
+const app = new Hono().basePath('/api');
 
-export default handle(app)
+// CORS setup
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "https://honoblog.vercel.app"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization", "x-api-key"],
+    exposeHeaders: ["Content-Length"],
+    credentials: true,
+    maxAge: 86400,
+  }),
+);
+
+app.get('/hello', (c) => c.json({ message: 'Hello from Hono! iw orks' }))
+
+export default app;
